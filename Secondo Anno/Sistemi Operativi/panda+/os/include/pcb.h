c1ef62f5ebf4b232572ffc68548575a1726994fe
Marco Coppola <marcocoppola02.mc@gmail.com>
2023-01-26 17:13:50 +0100

    Pulito il progetto

--- Secondo Anno/Sistemi Operativi/panda+/os/include/pcb.h
+++ Secondo Anno/Sistemi Operativi/panda+/os/include/pcb.h
@@ -4,7 +4,7 @@
 //
 
 /****************
- NOTE: quando possibile usare metodi static
+ NOTE: quando possibile usare metodi  
 ****************/
 
 #ifndef PHASE1_FILES_PCB_H
@@ -12,50 +12,51 @@
 
 #include "pandos_types.h"
 
+
 //Allocazione dei PCB
 
 // 1. Inizializza la lista pcbFree in modo da contenere tutti gli elementi della pcbFree_table.
 // Questo metodo deve essere chiamato una volta sola in fase di inizializzazione della struttura dati.
-static void initPcbs();
+  void initPcbs();
 
 // 2. Inserisce il PCB puntato da p nella lista dei PCB liberi (pcbFree_h)
-static void freePcb(pcb_t * p);
+  void freePcb(pcb_t * p);
 
 // 3. Restituisce NULL se la pcbFree_h è vuota. Altrimenti rimuove un elemento dalla pcbFree, inizializza tutti i campi (NULL/0) e restituisce l’elemento rimosso.
-static pcb_t *allocPcb();
+  pcb_t *allocPcb();
 
 // 4. Crea una lista di PCB, inizializzandola come lista vuota
-static void mkEmptyProcQ(struct list_head *head);
+  void mkEmptyProcQ(struct list_head *head);
 
 // 5. Restituisce TRUE se la lista puntata da head è vuota, FALSE altrimenti.
-static int emptyProcQ(struct list_head *head);
+  int emptyProcQ(struct list_head *head);
 
 // 6. Inserisce l’elemento puntato da p nella coda dei processi puntata da head.
-static void insertProcQ(struct list_head* head, pcb_t* p);
+  void insertProcQ(struct list_head* head, pcb_t* p);
 
 // 7. Restituisce l’elemento di testa della coda dei processi da head, SENZA RIMUOVERLO. Ritorna NULL se la coda non ha elementi.
-static pcb_t* headProcQ(struct list_head* head);
+  pcb_t* headProcQ(struct list_head* head);
 
 // 8. Rimuove il primo elemento dalla coda dei processi puntata da head. Ritorna NULL se la coda è vuota. Altrimenti ritorna il puntatore all’elemento rimosso dalla lista.
-static pcb_t* removeProcQ(struct list_head* head);
+  pcb_t* removeProcQ(struct list_head* head);
 
 // 9. Rimuove il PCB puntato da p dalla coda dei processi puntata da head. Se p non è presente nella coda, restituisce NULL.
 // (NOTA: p può trovarsi in una posizione arbitraria della coda).
-static pcb_t* outProcQ(struct list_head* head, pcb_t *p);
+  pcb_t* outProcQ(struct list_head* head, pcb_t *p);
 
 //Alberi di PCB
 
 // 10. Restituisce TRUE se il PCB puntato da p non ha figli, FALSE altrimenti
-static int emptyChild(pcb_t *p);
+  int emptyChild(pcb_t *p);
 
 // 11. Inserisce il PCB puntato da p come figlio del PCB puntato da prnt.
-static void insertChild(pcb_t *prnt,pcb_t *p);
+  void insertChild(pcb_t *prnt,pcb_t *p);
 
 // 12. Rimuove il primo figlio del PCB puntato da p. Se p non ha figli, restituisce NULL.
-static pcb_t* removeChild(pcb_t *p);
+  pcb_t* removeChild(pcb_t *p);
 
 // 13. Rimuove il PCB puntato da p dalla lista dei figli del padre. Se il PCB puntato da p non ha un padre, restituisce NULL, altrimenti restituisce l’elemento rimosso (cioè p).
 // A differenza della removeChild, p può trovarsi in una posizione arbitraria (ossia non è necessariamente il primo figlio del padre).
-static pcb_t* outChild(pcb_t* p);
+  pcb_t* outChild(pcb_t* p);
 
 #endif //PHASE1_FILES_PCB_H
--- Secondo Anno/Sistemi Operativi/panda+/os/src/pcb.c
+++ Secondo Anno/Sistemi Operativi/panda+/os/src/pcb.c
@@ -1,13 +1,13 @@
-//
+/ /
 // Created by Marco Coppola on 19/01/23.
 //
 
 #include "pcb.h"
 
 
-  struct list_head *pcb_free;
+struct list_head *pcb_free;
 
-  void initPcbs(){
+void initPcbs() {
 
     //Initialize the list of free PCBs
     INIT_LIST_HEAD(pcb_free);
@@ -15,47 +15,47 @@
     //TODO: needs more stuff
 }
 
-  void freePcb(pcb_t * p){
+void freePcb(pcb_t *p) {
 
     //Check if pcb points to null
-    if(p == NULL) //TODO: check if already in list
+    if (p == NULL) //TODO: check if already in list
         return;
 
     //Add element in list of free pcbs
     list_add(&p->p_list, pcb_free);
 }
 
-  pcb_t *allocPcb(){
+pcb_t *allocPcb() {
 
     //Returns null if list is empty, otherwhise gets first element from list
-    pcb_t* required = NULL; //OOOO
+    pcb_t *required = NULL; //OOOO
 
     //Remove from list if element is found
-    if(required != NULL){
+    if (required != NULL) {
         list_del(&required->p_list);
     }
 
     return required; //TODO: check if needs cleaining before return
 }
 
-  void mkEmptyProcQ(struct list_head *head){
+void mkEmptyProcQ(struct list_head *head) {
 
     //If head is null do nothing
-    if(head != NULL)
+    if (head != NULL)
         INIT_LIST_HEAD(head);
 }
 
-  int emptyProcQ(struct list_head *head){
+int emptyProcQ(struct list_head *head) {
 
     //If head is null we assume that is empty
-    if(head == NULL)
+    if (head == NULL)
         return 1;
     return list_empty(head);
 }
 
-  void insertProcQ(struct list_head* head, pcb_t* p){
+void insertProcQ(struct list_head *head, pcb_t *p) {
 
-    if(head != NULL && p != NULL){
-        list_add(&p->p_list,head);
+    if (head != NULL && p != NULL) {
+        list_add(&p->p_list, head);
     }
 }
