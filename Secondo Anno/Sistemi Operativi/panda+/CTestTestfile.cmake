82df2ac463ab83bb3e90e3a493b1ef3deffd4a0e
Marco Coppola <marcocoppola02.mc@gmail.com>
2023-02-18 09:56:57 +0100

    New readme

--- Secondo Anno/Sistemi Operativi/panda+/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/murkrow/a
-# Build directory: /home/murkrow/a
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("os")
-subdirs("phase1")
--- Secondo Anno/Sistemi Operativi/panda+/os/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/murkrow/a/os
-# Build directory: /home/murkrow/a/os
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
